7ff1a4ed1feceb54bf4e25c479077a36af80b881
DiegoSalas134 <diegosalas.dav13@gmail.com>
2025-06-15 14:29:00 -0600

    p1_f

--- BlackJ.cpp
+++ BlackJ.cpp
@@ -1,39 +1,82 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 //Estructuras de datos
 
+const int MaxCartas = 10;
+const int TamMazo= 52;
+
 //Estructura de carta
-struct Card
+struct Carta
 {
-    string value;
-    string figure;
-    int numValue;
+    string valor;
+    string figura;
+    int numValor;
 };
 
 //Estructura de Jugador
-struct Player
+struct Jugador
 {
-    string name;
-    int points;
-    int money;
-    vector<Card> cards; //vector de la mano del jugador
+    string nom;
+    int puntos;
+    int dinero;
+    Carta mano[MaxCartas];
+    int contCartas=0;
 };
 
 //Estructura de la partida en curso (historial)
 struct Match
 {
-    string nameOFplayer;
+    string nomJugador;
     string result;
-    string date;
+    string fecha;
 };
 
-//Creamos el mazo de cartas y lo mezclamos
-vector<Card> createDeck(){
+//funcion para crear mazo
+
+Carta* CrearMazo(){
+    Carta* mazo = new Carta[TamMazo];
+    string cartaValores[]={"2","3","4","5","6","7","8"
+    "9","10","J","K","Q","A"};
+    string cartaFiguras[]={"Diamantes","Treboles"
+    "Corazones","Picas"};
     
+    int inicio=0;
+    for (string figura: cartaFiguras)
+    {
+        for (string valor: cartaValores)
+        {
+            mazo[inicio].valor = valor;
+            mazo[inicio].figura= figura;
+
+            if (valor == "A")
+            {
+                mazo[inicio].numValor=11;
+            }
+            else if (valor=="J" || valor=="Q" || valor =="K")
+            {
+                mazo[inicio].numValor = 10;
+            }
+            else
+            {
+                mazo[inicio].numValor=stoi(valor);
+                //basicamente los valores que estan entre comillas
+                //ejemplo "2" lo convierte de string a int siendo ahora 2
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
+int main(){
+    srand(time(NULL));
+    return 0;
 }
