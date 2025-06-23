d53978d9f0ccaa07180f8a534c4775b93f0cd36a
sofismb06@gmail.com <sofismb06@gmail.com>
2025-06-23 05:34:32 +0300

    p3

--- BlackJ.cpp
+++ BlackJ.cpp
@@ -23,7 +23,7 @@ struct Carta
 struct Jugador
 {
     string nom;
-    int puntos;
+    int puntos=0;
     int dinero;
     Carta mano[MaxCartas];
     int contCartas=0;
@@ -90,6 +90,65 @@ void mezclarMazo(Carta* mazo){
     
 }
 
+int puntos(Jugador j){
+    int totalpuntos=0;
+    int ases=0;
+ for (int i = 0; i < j.contCartas; i++)
+    {
+        totalpuntos+=j.mano[i].numValor;//suma los puntos
+        if (j.mano[i].valor=="A")//casos de ases se suman
+        {
+            ases++;
+        }
+    }
+    
+    while (totalpuntos>21&&ases>0)
+    {
+        totalpuntos-=10;//se resta 10 para cambiar el valor a 1
+        ases--;//se decrementa la cantidad de ases
+    }
+    return totalpuntos;
+}
+
+//funcion para repartir cartas
+void repartir(Jugador &j, Carta* mazo, int &inicioMazo){
+    if (inicioMazo >=0 && j.contCartas <MaxCartas)
+    {
+        j.mano[j.contCartas]=mazo[inicioMazo];
+        j.contCartas++;
+    }
+
+   j.puntos=puntos(j);//se actualizan los puntos
+    inicioMazo--;//se elimina la carta
+}
+
+//funcion de cuando le toca al jugador
+void tuTurno(Jugador &j, Carta*mazo, int &inicioMazo){
+    char op;
+    do
+    {
+        cout <<"Tu mano, " << j.nom <<": "<<endl;
+        for (int i = 0; i < j.contCartas; i++)
+        {
+            cout << j.mano[i].valor << " de " << j.mano[i].figura <<endl;
+        }
+        cout << "Tus puntos son: " << j.puntos<<endl;
+        cout <<"¿Quieres otra carta? (s/n): "<<endl;
+        cin>>op;
+        if (op=='S' || op=='s')
+        {
+            repartir(j,mazo,inicioMazo);
+            if (j.puntos>21)
+            {
+                cout << "Te pasaste de 21"<<endl;
+                cout <<"Pierdes esta ronda"<<endl;
+                cout <<"Con un total de: " << j.puntos << " Puntos"<<endl;
+                break;
+            }
+        }
+    } while (op=='S' || op=='s');
+}
+
 
 int main(){
     srand(time(NULL));
@@ -99,7 +158,32 @@ int main(){
     for (int i = 0; i < 5; i++)
     {
         cout << mazo[i].valor << " de " << mazo[i].figura << endl;
-    }
+    } 
+    Jugador j;
+    int inicioMazo = TamMazo-1;//indice del mazo para saber que carta repartir
+    //-1 porque los arrays empiezan en 0
+    char op;
+    
+    do {
+        j = {"", 0, {}, 0};//valores segun el struct
+        cout << "--- BLACKJACK ---" << endl;
+        cout << "Dame tu nombre: ";
+        cin >> j.nom;
+        
+        repartir(j, mazo, inicioMazo);//se da carta numero 1
+        repartir(j, mazo, inicioMazo);//carta 2
+        
+        tuTurno(j, mazo, inicioMazo);
+        cout << "\nQuieres jugar otra vez? (s/n): ";
+        cin >> op;
+    } while(op=='S'||op== 's');
+        
+     
+        cout << "Juego realizado por: " << endl;
+        cout << "Ketzary" << endl;
+        cout << "Sofia" << endl;
+        cout << "Diego" << endl;
+
     
     return 0;
 }
